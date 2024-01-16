e96c68333adcd7f9fd2e785fc6619d9da504d377
Chistian Gilbertson <32468452+christiangil@users.noreply.github.com>
2024-01-15 21:36:31 -0700

    renaming to cost

--- src/strategies/minimize_skips.jl
+++ src/strategies/minimize_skips.jl
@@ -5,7 +5,7 @@ const too_many_skips = 2 * legal_max_skips + 1
 function squares_skipped(dice_roll::Int, ts::TrackState, track_locked::Bool; progress::Int=ts.progress)
     proposed_progress = dice_number_to_progress(dice_roll, ts)
     skips = (proposed_progress - progress) - 1
-    if track_locked || skips < 0 || (proposed_progress == 12 && ts.n_marked < 5)
+    if track_locked || skips < 0 || (proposed_progress == 12 && ts.n_marked < marks_for_track_lock)
         return too_many_skips  # throw out illegal moves
     end
     return skips
@@ -29,8 +29,8 @@ end
 
 
 # Matrices to hold the total amount of skips if you chose 2 dice. First dice x second dice (index is color dice then white dice for each color)
-const skips_2dice = zeros(2 * n_track_colors, n_track_colors) .+ too_many_skips
-const skips_2dice_rolls = zeros(Int, 2 * n_track_colors, n_track_colors)
+const costs_2dice = zeros(2 * n_track_colors, n_track_colors)
+const costs_2dice_rolls = zeros(Int, 2 * n_track_colors, n_track_colors)
 
 
 up_to_str(reversed::Bool) = reversed ? "down to" : "up to"
@@ -38,38 +38,38 @@ up_to_str(ts::TrackState) = up_to_str(ts.reversed)
 
 
 """
-    minimize_skips_strategy(gs, player_index; max_skip=1, penalty_max_skip=3)
+    minimize_skips_strategy(gs, player_index; max_cost=1, penalty_max_cost=3)
 
-A strategy defined by taking dice, as long as they dont skip more than `max_skip` spaces. Greedy in a point-sense.
+A strategy defined by taking dice, as long as they dont skip more than `max_cost` spaces. Greedy in a point-sense.
 """
-function minimize_skips_strategy(gs::GameState, player_index::Int; max_skip::Int=1, penalty_max_skip::Int=3, verbose::Bool=true, debug::Bool=false)
+function minimize_skips_strategy(gs::GameState, player_index::Int; max_cost::Int=1, penalty_max_cost::Int=3, verbose::Bool=true, debug::Bool=false)
     # TODO prioritize tracks with more marks
     @assert player_index <= length(gs.players)
     roll = gs.roll
     ps = gs.players[player_index]
     this_players_turn = player_index == gs.whose_turn
-    @assert 0 <= max_skip <= legal_max_skips
-    @assert 0 <= penalty_max_skip <= legal_max_skips
+    @assert 0 <= max_cost <= legal_max_skips
+    @assert 0 <= penalty_max_cost <= legal_max_skips
     white_roll = roll.white1 + roll.white2
 
     if this_players_turn
 
-        skips_2dice_rolls .= 0
-        skips_2dice .= too_many_skips
+        costs_2dice_rolls .= 0
+        costs_2dice .= too_many_skips
 
-        color_roll_skips_and_rolls= [color_roll_skips_helper(ps(color), roll(color), roll.white1, roll.white2, gs(color)) for color in track_colors]
-        color_roll_skips = [color_roll_skips_and_rolls[i][1] for i in eachindex(track_colors)]
-        color_rolls = [color_roll_skips_and_rolls[i][2] for i in eachindex(track_colors)]
-        white_roll_skips = [squares_skipped(white_roll, ps(color), gs(color)) for color in track_colors]
-        if debug; println(color_roll_skips, white_roll_skips) end
-        min_skip = min(minimum(color_roll_skips), minimum(white_roll_skips))
+        color_roll_costs_and_rolls= [color_roll_skips_helper(ps(color), roll(color), roll.white1, roll.white2, gs(color)) for color in track_colors]
+        color_roll_costs = [color_roll_costs_and_rolls[i][1] for i in eachindex(track_colors)]
+        color_rolls = [color_roll_costs_and_rolls[i][2] for i in eachindex(track_colors)]
+        white_roll_costs = [squares_skipped(white_roll, ps(color), gs(color)) for color in track_colors]
+        if debug; println(color_roll_costs, white_roll_costs) end
+        min_cost = min(minimum(color_roll_costs), minimum(white_roll_costs))
 
-        if min_skip > penalty_max_skip  # only take a dice if it is to our taste
+        if min_cost >= penalty_max_cost  # only take a dice if it is to our taste
             if verbose
-                if min_skip == too_many_skips
+                if min_cost == too_many_skips
                     println("Player $player_index is taking a penalty (couldn't take anything)") 
                 else
-                    println("Player $player_index is taking a penalty (would've had to skip $min_skip squares)") 
+                    println("Player $player_index is taking a penalty (would've had to skip $min_cost squares)") 
                 end
             end
             return add_penalty!(gs, ps)
@@ -77,48 +77,48 @@ function minimize_skips_strategy(gs::GameState, player_index::Int; max_skip::Int
 
         # check for best two dice taking combo
         for i in 1:n_track_colors  # first track, taking color first
-            if color_roll_skips[i] == min_skip  # only check second dice if you would take the first
-                for j in axes(skips_2dice, 2)  # second track
+            if color_roll_costs[i] == min_cost  # only check second dice if you would take the first
+                for j in axes(costs_2dice, 2)  # second track
                     if i == j 
-                        if white_roll_skips[j] > color_roll_skips[i]
-                            skips_2dice[i,j] = white_roll_skips[j]
+                        if white_roll_costs[j] > color_roll_costs[i]
+                            costs_2dice[i,j] = white_roll_costs[j]
                         end
                     else
-                        skips_2dice[i,j] = color_roll_skips[i] + white_roll_skips[j]
+                        costs_2dice[i,j] = color_roll_costs[i] + white_roll_costs[j]
                     end
                 end
             end
         end
         for i in (n_track_colors + 1):(2 * n_track_colors)  # first track, taking white first
-            if white_roll_skips[i - n_track_colors] == min_skip  # only check second dice if you would take the first
-                for j in axes(skips_2dice, 2)  # second track
+            if white_roll_costs[i - n_track_colors] == min_cost  # only check second dice if you would take the first
+                for j in axes(costs_2dice, 2)  # second track
                     if (i - n_track_colors) == j
-                        skips_2dice[i,j], skips_2dice_rolls[i, j] = color_roll_skips_helper(ps(j), roll(j), roll.white1, roll.white2, gs(j); progress=dice_number_to_progress(white_roll, ps(j)))
+                        costs_2dice[i,j], costs_2dice_rolls[i, j] = color_roll_skips_helper(ps(j), roll(j), roll.white1, roll.white2, gs(j); progress=dice_number_to_progress(white_roll, ps(j)))
                     else
-                        skips_2dice[i,j] = white_roll_skips[i - n_track_colors] + color_roll_skips[j]
+                        costs_2dice[i,j] = white_roll_costs[i - n_track_colors] + color_roll_costs[j]
                     end
                 end
             end
         end
-        min_skip_2d = minimum(skips_2dice)
+        min_cost_2d = minimum(costs_2dice)
 
-        # if second die is too skippy, only take one
-        if (min_skip_2d - min_skip) > max_skip
-            skips = Array{Int}(undef, n_track_colors)
+        # if second die is too costly, only take one
+        if (min_cost_2d - min_cost) >= max_cost
+            costs = Array{Int}(undef, n_track_colors)
             rolls = Array{Int}(undef, n_track_colors)
             is_white = Array{Bool}(undef, n_track_colors)
             for i in eachindex(track_colors)
-                if color_roll_skips[i] < white_roll_skips[i]
-                    skips[i] = color_roll_skips[i]
+                if color_roll_costs[i] < white_roll_costs[i]
+                    costs[i] = color_roll_costs[i]
                     rolls[i] = color_rolls[i]
                     is_white[i] = false
                 else
-                    skips[i] = white_roll_skips[i]
+                    costs[i] = white_roll_costs[i]
                     rolls[i] = white_roll
                     is_white[i] = true
                 end
             end
-            choice = rand([i for i in 1:length(skips) if skips[i]==min_skip])  # choose the track with the smallest skip
+            choice = rand([i for i in 1:length(costs) if costs[i]==min_cost])  # choose the track with the smallest cost
             if verbose
                 if is_white[choice]
                     println("Player $player_index is taking $(rolls[choice]) ($white_emoji$white_emoji) on $(color_emojis[choice])")
@@ -130,14 +130,11 @@ function minimize_skips_strategy(gs::GameState, player_index::Int; max_skip::Int
         end
 
         # take two dice
-        ci = CartesianIndices(skips_2dice)
-        skips_2dice_vec = vec(skips_2dice)  # might be unnecessary
-        min_inds = [i for i in eachindex(skips_2dice_vec) if (skips_2dice_vec[i] == min_skip_2d)]
+        ci = CartesianIndices(costs_2dice)
+        costs_2dice_vec = vec(costs_2dice)  # might be unnecessary
+        min_inds = [i for i in eachindex(costs_2dice_vec) if (costs_2dice_vec[i] == min_cost_2d)]
         choices = rand(ci[min_inds])
         choice1, choice2 = choices[1], choices[2]
-        # println(ci[min_inds])
-        # println(choice1, choice2)
-        # println(skips_2dice)
         if choice1 <= n_track_colors  # chose a color dice first
             if choice1 == choice2  # then chose white dice in same color
                 if verbose; println("Player $player_index is taking 2 die " * up_to_str(ps(choice2)) * " $white_roll on $(color_emojis[choice2]) ($(color_emojis[choice2])$white_emoji then $white_emoji$white_emoji)") end
@@ -150,8 +147,8 @@ function minimize_skips_strategy(gs::GameState, player_index::Int; max_skip::Int
         # chose white die first
         choice1 -= n_track_colors
         if choice1 == choice2  # then chose color dice in same color
-            if verbose; println("Player $player_index is taking 2 die " * up_to_str(ps(choice2)) * " $(skips_2dice_rolls[choice1 + n_track_colors, choice2]) on $(color_emojis[choice2]) ($white_emoji$white_emoji then $(color_emojis[choice2])$white_emoji)") end
-            return update_PlayerState!(gs, ps, choice2, skips_2dice_rolls[choice1 + n_track_colors, choice2]; n_marked_increase=2)
+            if verbose; println("Player $player_index is taking 2 die " * up_to_str(ps(choice2)) * " $(costs_2dice_rolls[choice1 + n_track_colors, choice2]) on $(color_emojis[choice2]) ($white_emoji$white_emoji then $(color_emojis[choice2])$white_emoji)") end
+            return update_PlayerState!(gs, ps, choice2, costs_2dice_rolls[choice1 + n_track_colors, choice2]; n_marked_increase=2)
         end
         if verbose; println("Player $player_index is taking $white_roll ($white_emoji$white_emoji) on $(color_emojis[choice1]) and $(color_rolls[choice2]) on $(color_emojis[choice2])") end
         update_PlayerState!(gs, ps, choice1, white_roll)
@@ -159,11 +156,11 @@ function minimize_skips_strategy(gs::GameState, player_index::Int; max_skip::Int
 
     else
 
-        skips = [squares_skipped(white_roll, ps(color), gs(color)) for color in track_colors]
-        if debug; println(skips) end
-        min_skip = minimum(skips)
-        if min_skip <= max_skip  # only take a dice if it is to our taste
-            choice = rand([i for i in 1:length(skips) if skips[i]==min_skip])  # choose the track with the smallest skip
+        costs = [squares_skipped(white_roll, ps(color), gs(color)) for color in track_colors]
+        if debug; println(costs) end
+        min_cost = minimum(costs)
+        if min_cost <= max_cost  # only take a dice if it is to our taste
+            choice = rand([i for i in 1:length(costs) if costs[i]==min_cost])  # choose the track with the smallest cost
             if verbose; println("Player $player_index is taking $white_roll ($white_emoji$white_emoji) on $(color_emojis[choice])") end
             return update_PlayerState!(gs, ps, choice, white_roll)
         elseif verbose
